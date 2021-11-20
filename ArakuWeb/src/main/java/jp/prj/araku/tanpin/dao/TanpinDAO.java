7fb71c63be0ff1b0753a60d9817e651c717640a5
LEE.JM <LEE.JM@DESKTOP-JLPVHCU>
2021-11-20 16:06:12 +0900

    오늘의 항목   - 엑셀 탬플릿작성시 빠진 부분 추가

--- ArakuWeb/src/main/java/jp/prj/araku/tanpin/dao/TanpinDAO.java
+++ ArakuWeb/src/main/java/jp/prj/araku/tanpin/dao/TanpinDAO.java
@@ -316,7 +316,7 @@ public class TanpinDAO {
 				}
 				
 				for(int i1=0; i1<subList.size(); i1++) {
-					sheet = workbook.createSheet((i1+1)+"シート");
+					sheet = workbook.createSheet("シート"+(i1+1));
 					// 컬럼너비 세팅
 					sheet.setDefaultColumnWidth(12);
 					// 눈금선 없애기
@@ -352,6 +352,7 @@ public class TanpinDAO {
 					centerAllLine.setBorderLeft(BorderStyle.THICK);
 					centerAllLine.setBorderRight(BorderStyle.THICK);
 					centerAllLine.setAlignment(HorizontalAlignment.CENTER);
+					centerAllLine.setVerticalAlignment(VerticalAlignment.CENTER);
 					
 					row = sheet.createRow(4);
 					sheet.addMergedRegion(new CellRangeAddress(4,4,7,8));
@@ -437,8 +438,20 @@ public class TanpinDAO {
 					}
 					row = sheet.createRow(16);
 					sheet.addMergedRegion(new CellRangeAddress(16,16,1,8));
+					sheet.addMergedRegion(new CellRangeAddress(16,17,0,0));
+					sheet.addMergedRegion(new CellRangeAddress(17,17,1,8));
 					cell = row.createCell(0);
 					cell.setCellValue("住所");
+					cell.setCellStyle(centerAllLine);
+					cell = row.createCell(1);
+					cell.setCellValue("〒 "+dealerInfo.get(0).getDealer_post());
+					cell.setCellStyle(allLine);
+					for(int c=2; c<=8; c++) {
+						cell = row.createCell(c);
+						cell.setCellStyle(allLine);
+					}
+					row = sheet.createRow(17);
+					cell = row.createCell(0);
 					cell.setCellStyle(centerAllLine);
 					cell = row.createCell(1);
 					cell.setCellValue(dealerInfo.get(0).getDealer_add());
@@ -447,8 +460,8 @@ public class TanpinDAO {
 						cell = row.createCell(c);
 						cell.setCellStyle(allLine);
 					}
-					row = sheet.createRow(17);
-					sheet.addMergedRegion(new CellRangeAddress(17,17,1,8));
+					row = sheet.createRow(18);
+					sheet.addMergedRegion(new CellRangeAddress(18,18,1,8));
 					cell = row.createCell(0);
 					cell.setCellValue("TEL");
 					cell.setCellStyle(centerAllLine);
@@ -459,8 +472,8 @@ public class TanpinDAO {
 						cell = row.createCell(c);
 						cell.setCellStyle(allLine);
 					}
-					row = sheet.createRow(18);
-					sheet.addMergedRegion(new CellRangeAddress(18,18,1,8));
+					row = sheet.createRow(19);
+					sheet.addMergedRegion(new CellRangeAddress(19,19,1,8));
 					cell = row.createCell(0);
 					cell.setCellValue("納品希望日");
 					cell.setCellStyle(centerAllLine);
@@ -471,10 +484,22 @@ public class TanpinDAO {
 						cell = row.createCell(c);
 						cell.setCellStyle(allLine);
 					}
-					row = sheet.createRow(19);
-					sheet.addMergedRegion(new CellRangeAddress(19,19,1,8));
+					row = sheet.createRow(20);
+					sheet.addMergedRegion(new CellRangeAddress(20,20,1,8));
+					sheet.addMergedRegion(new CellRangeAddress(20,21,0,0));
+					sheet.addMergedRegion(new CellRangeAddress(21,21,1,8));
 					cell = row.createCell(0);
 					cell.setCellValue("備考");
+					cell.setCellStyle(centerAllLine);
+					cell = row.createCell(1);
+					cell.setCellValue("発注書No.??");
+					cell.setCellStyle(allLine);
+					for(int c=2; c<=8; c++) {
+						cell = row.createCell(c);
+						cell.setCellStyle(allLine);
+					}
+					row = sheet.createRow(21);
+					cell = row.createCell(0);
 					cell.setCellStyle(centerAllLine);
 					cell = row.createCell(1);
 					cell.setCellValue(dealerInfo.get(0).getRemark());
@@ -483,11 +508,11 @@ public class TanpinDAO {
 						cell = row.createCell(c);
 						cell.setCellStyle(allLine);
 					}
-					row = sheet.createRow(20);
+					row = sheet.createRow(22);
 					
-					row = sheet.createRow(21);
-					sheet.addMergedRegion(new CellRangeAddress(21,21,0,2));
-					sheet.addMergedRegion(new CellRangeAddress(21,21,4,5));
+					row = sheet.createRow(23);
+					sheet.addMergedRegion(new CellRangeAddress(23,23,0,2));
+					sheet.addMergedRegion(new CellRangeAddress(23,23,4,5));
 					cell = row.createCell(0);
 					cell.setCellValue("商品名");
 					cell.setCellStyle(centerAllLine);
@@ -515,9 +540,9 @@ public class TanpinDAO {
 					List<JaikoPrdInventoryVO> innerList = subList.get(i1);
 					int finalLine = 0;
 					for(int i2=0; i2<innerList.size(); i2++) {
-						row = sheet.createRow(i2+22);
-						sheet.addMergedRegion(new CellRangeAddress(i2+22,i2+22,0,2));
-						sheet.addMergedRegion(new CellRangeAddress(i2+22,i2+22,4,5));
+						row = sheet.createRow(i2+24);
+						sheet.addMergedRegion(new CellRangeAddress(i2+24,i2+24,0,2));
+						sheet.addMergedRegion(new CellRangeAddress(i2+24,i2+24,4,5));
 						JaikoPrdInventoryVO innerVO = innerList.get(i2);
 						cell = row.createCell(0);
 						cell.setCellValue(innerVO.getPrd_nm());
@@ -543,7 +568,7 @@ public class TanpinDAO {
 						cell = row.createCell(8);
 						cell.setCellValue("備考");
 						cell.setCellStyle(allLine);
-						finalLine = i2+22;
+						finalLine = i2+24;
 					}
 					row = sheet.createRow(finalLine+1);
 					row = sheet.createRow(finalLine+2);
@@ -558,9 +583,273 @@ public class TanpinDAO {
 					cell.setCellStyle(allLine);
 					sheet.addMergedRegion(new CellRangeAddress(finalLine+2,finalLine+4,0,0));
 				}
+			}else {
+				sheet = workbook.createSheet("シート1");
+				// 컬럼너비 세팅
+				sheet.setDefaultColumnWidth(12);
+				// 눈금선 없애기
+				sheet.setDisplayGridlines(false);
+				row = sheet.createRow(0);
+				
+				// 타이틀세팅
+				row = sheet.createRow(1);
+				sheet.addMergedRegion(new CellRangeAddress(1,2,0,8));
+				cell = row.createCell(0);
+				cell.setCellStyle(title);
+				cell.setCellValue("発注書");
+				for(int c=1; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(title);
+				}
+				row = sheet.createRow(2);
+				for(int c=0; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(title);
+				}
+				row = sheet.createRow(3);
+				
+				CellStyle allLine = workbook.createCellStyle();
+				allLine.setBorderBottom(BorderStyle.THICK);
+				allLine.setBorderTop(BorderStyle.THICK);
+				allLine.setBorderLeft(BorderStyle.THICK);
+				allLine.setBorderRight(BorderStyle.THICK);
+				
+				CellStyle centerAllLine = workbook.createCellStyle();
+				centerAllLine.setBorderBottom(BorderStyle.THICK);
+				centerAllLine.setBorderTop(BorderStyle.THICK);
+				centerAllLine.setBorderLeft(BorderStyle.THICK);
+				centerAllLine.setBorderRight(BorderStyle.THICK);
+				centerAllLine.setAlignment(HorizontalAlignment.CENTER);
+				centerAllLine.setVerticalAlignment(VerticalAlignment.CENTER);
+				
+				row = sheet.createRow(4);
+				sheet.addMergedRegion(new CellRangeAddress(4,4,7,8));
+				cell = row.createCell(6);
+				cell.setCellValue("発注日");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(7);
+				cell.setCellValue(CommonUtil.getDate("YYYY/MM/dd", 0));
+				cell.setCellStyle(allLine);
+				cell = row.createCell(8);
+				cell.setCellStyle(allLine);
+				
+				CellStyle underLine = workbook.createCellStyle();
+				underLine.setBorderBottom(BorderStyle.THICK);
+				row = sheet.createRow(5);
+				sheet.addMergedRegion(new CellRangeAddress(5,5,0,1));
+				sheet.addMergedRegion(new CellRangeAddress(5,5,7,8));
+				cell = row.createCell(0);
+				cell.setCellValue(dealerInfo.get(0).getDealer_nm()+" 御中");
+				cell.setCellStyle(underLine);
+				cell = row.createCell(1);
+				cell.setCellStyle(underLine);
+				cell = row.createCell(6);
+				cell.setCellValue("発注者");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(7);
+				cell.setCellValue("金 大元");
+				cell.setCellStyle(allLine);
+				cell = row.createCell(8);
+				cell.setCellStyle(allLine);
+				row = sheet.createRow(6);
+				row = sheet.createRow(7);
+				sheet.addMergedRegion(new CellRangeAddress(7,7,0,1));
+				cell = row.createCell(0);
+				cell.setCellValue("FAX："+dealerInfo.get(0).getDealer_fax());
+				cell.setCellStyle(underLine);
+				cell = row.createCell(1);
+				cell.setCellStyle(underLine);
+				row = sheet.createRow(8);
+				sheet.addMergedRegion(new CellRangeAddress(8,8,5,8));
+				cell = row.createCell(5);
+				cell.setCellValue("有限会社アイテムピアジャパン");
+				row = sheet.createRow(9);
+				sheet.addMergedRegion(new CellRangeAddress(9,9,5,8));
+				cell = row.createCell(5);
+				cell.setCellValue("〒333－0845");
+				row = sheet.createRow(10);
+				sheet.addMergedRegion(new CellRangeAddress(10,10,0,3));
+				sheet.addMergedRegion(new CellRangeAddress(10,10,5,8));
+				cell = row.createCell(0);
+				cell.setCellValue("いつも大変お世話になっております。");
+				cell = row.createCell(5);
+				cell.setCellValue("埼玉県川口市上青木西1-19-39");
+				row = sheet.createRow(11);
+				sheet.addMergedRegion(new CellRangeAddress(11,11,0,3));
+				sheet.addMergedRegion(new CellRangeAddress(11,11,5,8));
+				cell = row.createCell(0);
+				cell.setCellValue("下記内容お手配いただけますようお願い申し上げます。");
+				cell = row.createCell(5);
+				cell.setCellValue("滝沢ビル１階");
+				row = sheet.createRow(12);
+				sheet.addMergedRegion(new CellRangeAddress(12,12,5,8));
+				cell = row.createCell(5);
+				cell.setCellValue("TEL：048-242-3801");
+				row = sheet.createRow(13);
+				sheet.addMergedRegion(new CellRangeAddress(13,13,5,8));
+				cell = row.createCell(5);
+				cell.setCellValue("FAX：048-242-3802");
+				row = sheet.createRow(14);
+				
+				// 거래처 정보
+				row = sheet.createRow(15);
+				sheet.addMergedRegion(new CellRangeAddress(15,15,1,8));
+				cell = row.createCell(0);
+				cell.setCellValue("納品先");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellValue(dealerInfo.get(0).getDestination());
+				cell.setCellStyle(allLine);
+				for(int c=2; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(allLine);
+				}
+				row = sheet.createRow(16);
+				sheet.addMergedRegion(new CellRangeAddress(16,16,1,8));
+				sheet.addMergedRegion(new CellRangeAddress(16,17,0,0));
+				sheet.addMergedRegion(new CellRangeAddress(17,17,1,8));
+				cell = row.createCell(0);
+				cell.setCellValue("住所");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellValue("〒 "+dealerInfo.get(0).getDealer_post());
+				cell.setCellStyle(allLine);
+				for(int c=2; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(allLine);
+				}
+				row = sheet.createRow(17);
+				cell = row.createCell(0);
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellValue(dealerInfo.get(0).getDealer_add());
+				cell.setCellStyle(allLine);
+				for(int c=2; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(allLine);
+				}
+				row = sheet.createRow(18);
+				sheet.addMergedRegion(new CellRangeAddress(18,18,1,8));
+				cell = row.createCell(0);
+				cell.setCellValue("TEL");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellValue(dealerInfo.get(0).getDealer_tel());
+				cell.setCellStyle(allLine);
+				for(int c=2; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(allLine);
+				}
+				row = sheet.createRow(19);
+				sheet.addMergedRegion(new CellRangeAddress(19,19,1,8));
+				cell = row.createCell(0);
+				cell.setCellValue("納品希望日");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellValue(dealerInfo.get(0).getEst_delivery_dt());
+				cell.setCellStyle(allLine);
+				for(int c=2; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(allLine);
+				}
+				row = sheet.createRow(20);
+				sheet.addMergedRegion(new CellRangeAddress(20,20,1,8));
+				sheet.addMergedRegion(new CellRangeAddress(20,21,0,0));
+				sheet.addMergedRegion(new CellRangeAddress(21,21,1,8));
+				cell = row.createCell(0);
+				cell.setCellValue("備考");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellValue("発注書No.??");
+				cell.setCellStyle(allLine);
+				for(int c=2; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(allLine);
+				}
+				row = sheet.createRow(21);
+				cell = row.createCell(0);
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellValue(dealerInfo.get(0).getRemark());
+				cell.setCellStyle(allLine);
+				for(int c=2; c<=8; c++) {
+					cell = row.createCell(c);
+					cell.setCellStyle(allLine);
+				}
+				row = sheet.createRow(22);
+				
+				row = sheet.createRow(23);
+				sheet.addMergedRegion(new CellRangeAddress(23,23,0,2));
+				sheet.addMergedRegion(new CellRangeAddress(23,23,4,5));
+				cell = row.createCell(0);
+				cell.setCellValue("商品名");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(1);
+				cell.setCellStyle(allLine);
+				cell = row.createCell(2);
+				cell.setCellStyle(allLine);
+				cell = row.createCell(3);
+				cell.setCellValue("入数");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(4);
+				cell.setCellValue("数量");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(5);
+				cell.setCellStyle(allLine);
+				cell = row.createCell(6);
+				cell.setCellValue("単価");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(7);
+				cell.setCellValue("合計金額");
+				cell.setCellStyle(centerAllLine);
+				cell = row.createCell(8);
+				cell.setCellValue("備考");
+				cell.setCellStyle(centerAllLine);
+				int finalLine = 0;
+				for(int i2=0; i2<list.size(); i2++) {
+					row = sheet.createRow(i2+24);
+					sheet.addMergedRegion(new CellRangeAddress(i2+24,i2+24,0,2));
+					sheet.addMergedRegion(new CellRangeAddress(i2+24,i2+24,4,5));
+					JaikoPrdInventoryVO innerVO = list.get(i2);
+					cell = row.createCell(0);
+					cell.setCellValue(innerVO.getPrd_nm());
+					cell.setCellStyle(allLine);
+					cell = row.createCell(1);
+					cell.setCellStyle(allLine);
+					cell = row.createCell(2);
+					cell.setCellStyle(allLine);
+					cell = row.createCell(3);
+					cell.setCellValue("入数");
+					cell.setCellStyle(allLine);
+					cell = row.createCell(4);
+					cell.setCellValue("数量");
+					cell.setCellStyle(allLine);
+					cell = row.createCell(5);
+					cell.setCellStyle(allLine);
+					cell = row.createCell(6);
+					cell.setCellValue(innerVO.getPrd_unit_prc());
+					cell.setCellStyle(allLine);
+					cell = row.createCell(7);
+					cell.setCellValue("合計金額");
+					cell.setCellStyle(allLine);
+					cell = row.createCell(8);
+					cell.setCellValue("備考");
+					cell.setCellStyle(allLine);
+					finalLine = i2+24;
+				}
+				row = sheet.createRow(finalLine+1);
+				row = sheet.createRow(finalLine+2);
+				cell = row.createCell(0);
+				cell.setCellValue("通信欄");
+				cell.setCellStyle(centerAllLine);
+				row = sheet.createRow(finalLine+3);
+				cell = row.createCell(0);
+				cell.setCellStyle(allLine);
+				row = sheet.createRow(finalLine+4);
+				cell = row.createCell(0);
+				cell.setCellStyle(allLine);
+				sheet.addMergedRegion(new CellRangeAddress(finalLine+2,finalLine+4,0,0));
 			}
-			
-			
 			
 			String headerKey = "Content-Disposition";
 			String headerValue = String.format("attachment;filename=\"%s\"",
