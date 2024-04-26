937796a823b3d64bb5a9f9276c60300c9c146b18
LEE.JM <LEE.JM@DESKTOP-JLPVHCU>
2024-04-27 00:43:59 +0900

    재고 / 아라쿠 인터셉터기능 살리기

--- ArakuWeb/src/main/java/jp/prj/araku/interceptor/ArakuLoginInterceptor.java
+++ ArakuWeb/src/main/java/jp/prj/araku/interceptor/ArakuLoginInterceptor.java
@@ -2,6 +2,7 @@ package jp.prj.araku.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,17 @@ public class ArakuLoginInterceptor extends HandlerInterceptorAdapter {
 			throws Exception {
 		log.debug("preHandle2");
 		log.debug(request.getRequestURI());
+		
+		HttpSession session = request.getSession();
+		String userId = (String) session.getAttribute("arakuId");
+		log.debug("-----------------------------");
+		log.debug("arakuId : {}", userId);
+		log.debug("-----------------------------");
+		
+		if(userId == null) {
+			response.sendRedirect("/araku");
+			return false;
+		}
 		return true;
 	}
 }
